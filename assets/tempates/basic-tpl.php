815b7c84762bf3681190183ed5788d1a34d3125a
Jon Zielenkievicz <jzielenkievicz@nycgo.com>
2015-08-12 17:04:51 -0400

    update email styles

--- assets/tempates/basic-tpl.php
+++ assets/tempates/basic-tpl.php
@@ -8,55 +8,72 @@
     -ms-text-size-adjust:none;text-size-adjust:none;} /* ipad text fix */
   </style>
 </head>
-<body style="margin:0;padding:0;background-color:#F4F4F4;">
-    <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="background-color:#F4F4F4;">
+<body style="margin:0;padding:0;background-color:#ededed;">
+    <table cellspacing="0" cellpadding="0" border="0" width="100%" style="background-color:#ededed;">
         <tr>
-            <td>
-                <a href="{url}"><img src="{headerImage}" alt="{headerImageAlt}" style="display:block;width:100%;" /></a>
-            </td>
-        </tr>
-        <tr>
-            <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+            <td height="10" style="height:10px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
         </tr>
         <tr>
             <td>
-                <table cellspacing="0" cellpadding="0" border="0" style="width:90%;margin:0 auto;background-color:#fff;">
+                <table cellspacing="0" cellpadding="0" border="0" width="100%" style="background-color:#ededed;">
                     <tr>
-                        <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
-                    </tr>
-                    <tr>
+                        <td width="10"></td>
                         <td>
-                            <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="background-color:#F4F4F4;">
                                 <tr>
-                                    <td width="20"></td>
                                     <td>
-                                        <span style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#000;">Hi!</span><br /><br />
-                                        <span style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#000;">{message}</span>
+                                        <a href="{url}"><img src="{headerImage}" alt="{headerImageAlt}" style="display:block;width:100%;" /></a>
                                     </td>
-                                    <td width="20"></td>
+                                </tr>
+                                <tr>
+                                    <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <table cellspacing="0" cellpadding="0" border="0" style="width:90%;margin:0 auto;background-color:#fff;">
+                                            <tr>
+                                                <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                                        <tr>
+                                                            <td width="20"></td>
+                                                            <td>
+                                                                <span style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#000;">Hi!</span><br /><br />
+                                                                <span style="font-family:Arial,Helvetica,sans-serif;font-size:14px;color:#000;">{message}</span>
+                                                            </td>
+                                                            <td width="20"></td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td height="20" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align:center;">
+                                        <span style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#999;">&copy; <?php echo date('Y'); ?>, Jon Z.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
                                 </tr>
                             </table>
                         </td>
-                    </tr>
-                    <tr>
-                        <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+                        <td width="10"></td>
                     </tr>
                 </table>
             </td>
         </tr>
         <tr>
-            <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
-        </tr>
-        <tr>
-            <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
-        </tr>
-        <tr>
-            <td style="text-align:center;">
-                <span style="font-family:Arial,Helvetica,sans-serif;font-size:12px;color:#999;">&copy; <?php echo date('Y'); ?>, Jon Z.</span>
-            </td>
-        </tr>
-        <tr>
-            <td height="14" style="height:14px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
+            <td height="10" style="height:10px !important;line-height:1px;font-size:0px;"><span style="font-size:0;line-height:0;">&nbsp;</span></td>
         </tr>
     </table>
 </body>
